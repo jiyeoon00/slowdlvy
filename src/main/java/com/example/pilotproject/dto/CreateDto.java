4324526e1b6cfdb76337ffe3ac5eac8658a29765
NamYeju <dpwn0112@naver.com>
2022-05-22 13:56:31 +0900

    chore: dto 수정

--- src/main/java/com/example/pilotproject/dto/CreateDto.java
+++ src/main/java/com/example/pilotproject/dto/CreateDto.java
@@ -1,8 +1,19 @@
 package com.example.pilotproject.dto;
 
+import com.example.pilotproject.entity.State;
+import com.example.pilotproject.entity.Todo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateDto {
     private String content;
+
+    public Todo toEntity(){
+        return Todo.builder()
+                .content(content)
+                .state(State.ACTIVE)
+                .build();
+    }
 }
--- src/main/java/com/example/pilotproject/dto/UpdateDto.java
+++ src/main/java/com/example/pilotproject/dto/UpdateDto.java
@@ -1,7 +1,9 @@
 package com.example.pilotproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class UpdateDto {
     private String content;
