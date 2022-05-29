3ae80859d3c4c74d5d35530fdb431353dfcfed81
9keyyyy <9keyyyy@gmail.com>
2022-05-29 11:48:53 +0900

    Feat : 비관적 락 추가

--- pilot_todo/src/main/java/com/example/demo/repository/TodoRepository.java
+++ pilot_todo/src/main/java/com/example/demo/repository/TodoRepository.java
@@ -2,13 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.TodoVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoVO, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select t from Todo t where t.id = :id")
+    Optional<TodoVO> findById(Long id);
 }
