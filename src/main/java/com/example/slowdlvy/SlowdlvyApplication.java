ae8cf5c4d56f5b138d2486c796cd5376a710944d
jiyeon <sjy6736@naver.com>
2022-08-01 06:01:14 +0900

    initial commit

---
+++ src/main/java/com/example/slowdlvy/SlowdlvyApplication.java
@@ -0,0 +1,13 @@
+package com.example.slowdlvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowdlvyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowdlvyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slowdlvy/SlowdlvyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slowdlvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowdlvyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
